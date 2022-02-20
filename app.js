122f8a8d1d4920eb71f8d1d5a1d4d3a182cff745
Jarod Florence <jarod.florence@outlook.com>
2022-02-20 12:58:33 -0600

    validation added

--- app.js
+++ app.js
@@ -7,12 +7,26 @@ const promptUser = () => {
     {
       type: 'input',
       name: 'name',
-      message: 'What is your name?'
+      message: 'What is your name? (Required)',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your name!');
+        }
+      }
     },
     {
       type: 'input',
       name: 'github',
-      message: 'Enter your GitHub Username'
+      message: 'Enter your GitHub Username (Required)',
+      validate: gitNameInput => {
+        if (gitNameInput) {
+          return true;
+        } else {
+          console.log('Please enter your GitHub username!');
+        }
+      }
     }, 
     {
       type: 'input',
@@ -37,12 +51,26 @@ Add a New Project
     {
       type: 'input',
       name: 'name',
-      message: 'What is the name of your project?'
+      message: 'What is the name of your project? (Required)',
+      validate: projectInput => {
+        if (projectInput) {
+          return true;
+        } else {
+          console.log('Please enter your project name!');
+        }
+      }
     },
     {
       type: 'input',
       name: 'description',
-      message: 'Provide a description of the project (Required)'
+      message: 'Provide a description of the project (Required)',
+      validate: descrInput => {
+        if (descrInput) {
+          return true;
+        } else {
+          console.log('Please describe your project!');
+        }
+      }
     },
     {
       type: 'checkbox',
@@ -53,7 +81,14 @@ Add a New Project
     {
       type: 'input',
       name: 'link',
-      message: 'Enter the GitHub link to your project. (Required)'
+      message: 'Enter the GitHub link to your project. (Required)',
+      validate: linkInput => {
+        if (linkInput) {
+          return true;
+        } else {
+          console.log('Please enter the link to your project!');
+        }
+      }
     },
     {
       type: 'confirm',
